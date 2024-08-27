b8c7ed9c6f43878e7cd68da618e2e929085ca8a6
JoshuaFoster123 <joshyfostet@gmail.com>
2024-08-27 16:45:00 +1000

    Cloudflare addition

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,55 +11,45 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 import react from "@astrojs/react";
 
 /** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+import cloudflare from "@astrojs/cloudflare";
 const astroExpressiveCodeOptions = {
-  themes: ["min-dark", "min-light"],
+  themes: ["min-dark", "min-light"]
 };
 
+// https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "prepend",
-          content: {
-            type: "text",
-            value: "#",
-          },
-          headingProperties: {
-            className: ["anchor"],
-          },
-          properties: {
-            className: ["anchor-link mr-5"],
-          },
-        },
-      ],
-    ],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: "prepend",
+      content: {
+        type: "text",
+        value: "#"
+      },
+      headingProperties: {
+        className: ["anchor"]
+      },
+      properties: {
+        className: ["anchor-link mr-5"]
+      }
+    }]]
   },
-  integrations: [
-    (await import("astro-compress")).default({
-      CSS: false,
-      SVG: false,
-    }),
-    tailwind(),
-    sitemap(),
-    expressiveCode(astroExpressiveCodeOptions),
-    icon(),
-    mdx(),
-    react(),
-  ],
+  integrations: [(await import("astro-compress")).default({
+    CSS: false,
+    SVG: false
+  }), tailwind(), sitemap(), expressiveCode(astroExpressiveCodeOptions), icon(), mdx(), react()],
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
   site: "https://astrozinc.exylons.com",
   vite: {
     plugins: [rawFonts([".ttf", ".woff"])],
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
+      exclude: ["@resvg/resvg-js"]
+    }
   },
+  output: "server",
+  adapter: cloudflare()
 });
 
 // vite plugin to import fonts
@@ -67,13 +57,13 @@ function rawFonts(ext) {
   return {
     name: "vite-plugin-raw-fonts",
     transform(_, id) {
-      if (ext.some((e) => id.endsWith(e))) {
+      if (ext.some(e => id.endsWith(e))) {
         const buffer = readFileSync(id);
         return {
           code: `export default ${JSON.stringify(buffer)}`,
-          map: null,
+          map: null
         };
       }
-    },
+    }
   };
 }
