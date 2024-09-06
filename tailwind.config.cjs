c3e1bc1792a506d3b222a14bcc0a5ac256cff18f
JoshuaFoster123 <joshyfostet@gmail.com>
2024-09-06 15:31:44 +1000

    Post update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,astro,mdx}"],
   darkMode: "class",
   theme: {
     extend: {
